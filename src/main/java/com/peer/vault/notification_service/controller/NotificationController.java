df28919c52e506553e4dfed4fdefe710d9e58d0f
Manish Kumar Choudhary <manish@platformcommons.com>
2024-08-04 00:34:25 +0530

    No hanges

--- src/main/java/com/peer/vault/notification_service/controller/NotificationController.java
+++ src/main/java/com/peer/vault/notification_service/controller/NotificationController.java
@@ -1,14 +1,10 @@
 package com.peer.vault.notification_service.controller;
 
-import com.netflix.discovery.converters.Auto;
 import com.peer.vault.notification_service.dto.EmailRequest;
 import com.peer.vault.notification_service.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notifications")
